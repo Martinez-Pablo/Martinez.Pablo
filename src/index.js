fe5bd8b03cd1b04f1801ceaa140df35ed18671fc
Pablo Martinez <martinezpabloagustin1@gmail.com>
2022-11-27 21:39:34 -0300

    Tercera Preentrega

---
+++ src/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarProductos(productos);
+
+    if (localStorage.getItem("carrito")) {
+        const carrito = obtenerCarritoStorage();
+        pintarCarrito(carrito);
+        actualizarTotalesCarrito(carrito);
+    };
+});
---
+++ src/stock.js
@@ -0,0 +1,58 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Duo",
+        tipo: "Hamburguesa",
+        desc: "Dos medallones de carne, pan de papa, mostaza, ketchup, queso derretido, cebolla picada",
+        precio: 900,
+        img: "./public/imagenes/1.png",
+        cantidad:1
+    },
+    {
+        id: 2,
+        nombre: "Big",
+        tipo: "Hamburguesa",
+        desc: "Dos medallon de carne, pan de papa, cebolla picada, lechuga, pepino, queso derretido",
+        precio: 950,
+        img: "./public/imagenes/2.png",
+        cantidad:1
+    },
+    {
+        id: 3,
+        nombre: "Magnifica",
+        tipo: "Hamburguesa",
+        desc: "Medallon de carne, pan de papa, queso cheddar, tomate, lechuga, cebolla",
+        precio: 850,
+        img: "./public/imagenes/3.png",
+        cantidad:1
+    },
+    {
+        id: 4,
+        nombre: "Grand Tasty",
+        tipo: "Hamburguesa",
+        desc: "Dos medallones de carne, pan de papa con sesamo, triple queso cheddar, cebolla, tomate, lechuga, salsa tasty",
+        precio: 1000,
+        img: "./public/imagenes/4.png",
+        cantidad:1
+    },
+    {
+        id: 5,
+        nombre: "Doble Bacon",
+        tipo: "Hamburguesa",
+        desc: "Dos medallones de carne, pan de papa con sesamo, queso cheddar, cebolla, bacon, mostaza, ketchup",
+        precio: 950,
+        img: "./public/imagenes/5.png",
+        cantidad:1
+    },
+    {
+        id: 6,
+        nombre: "Tasty Turbo Bacon",
+        tipo: "Hamburguesa",
+        desc: "Dos medallones de carne, pan de papa con sesamo, triple queso cheddar, cebolla, tomate, lechuga, salsa tasty, extra adicional de panceta",
+        precio: 1200,
+        img: "./public/imagenes/6.png",
+        cantidad:1
+    }
+    
+]
